118e48c06f993313bdb95b37527ae997581b5408
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-07 15:57:02 +0100

    feat: exercise completed

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const imageLamp = document.getElementById("lamp");
+const bntElem = document.getElementById("light-bnt")
+
+bntElem.addEventListener("click", function() {
+    imageLamp.src="./img/yellow_lamp.png";
+})
