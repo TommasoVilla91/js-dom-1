f12b574d5d822047f14689de389217cda2ab9041
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-07 16:30:03 +0100

    feat: bonus completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const imageLamp = document.getElementById("lamp");
 const bntElem = document.getElementById("light-bnt")
 
+
 bntElem.addEventListener("click", function() {
-    imageLamp.src="./img/yellow_lamp.png";
+    // imageLamp.src="./img/yellow_lamp.png";
+
+    if (imageLamp.src.includes("white_lamp.png")) {
+        imageLamp.src="./img/yellow_lamp.png";
+        bntElem.innerHTML = "Spegni"
+    } else {
+        imageLamp.src="./img/white_lamp.png";
+        bntElem.innerHTML = "Accendi"
+    }
 })
